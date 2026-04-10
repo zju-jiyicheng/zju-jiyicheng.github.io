1ba147164591c88cfa6ceb01d5c6a6ce0dbed61a
zju-jiyicheng <2457575148@qq.com>
2026-04-10 15:49:35 +0800

    Add publications

--- assets/site-data.js
+++ assets/site-data.js
@@ -1,12 +1,12 @@
 window.SITE_DATA = {
   profile: {
-    name: "Yicheng JI",
+    name: "Yicheng Ji",
     nativeName: "季奕丞",
     headline: "PhD Student @ Zhejiang University",
     avatar: "assets/me.jpg",
     intro: [
       {
-        html: 'Hi!👋 I am Yicheng Ji (季奕丞). I am currently a PhD student at Zhejiang University, advised by Prof. <a href="https://scholar.google.com/citations?user=fZoYJz8AAAAJ&hl=en&authuser=2" target="_blank" rel="noreferrer">Huan LI</a>. I also work closely with <a href="https://scholar.google.com/citations?user=JSvMHnEAAAAJ&hl=en&authuser=2" target="_blank" rel="noreferrer">Jun Zhang</a>. My academic interests are centered on efficient visual understanding and video generation.'
+        html: 'Hi!👋 I am Yicheng Ji (季奕丞). I am currently a PhD student at Zhejiang University, advised by Prof. <a href="https://scholar.google.com/citations?user=fZoYJz8AAAAJ&hl=en&authuser=2" target="_blank" rel="noreferrer">Huan Li</a>. I also work closely with <a href="https://scholar.google.com/citations?user=JSvMHnEAAAAJ&hl=en&authuser=2" target="_blank" rel="noreferrer">Jun Zhang</a>. My academic interests are centered on efficient visual understanding and video generation.'
       },
       "Before starting my doctoral study, I completed my undergraduate education at the Turing Class, Chu Kochen Honors College (竺可桢学院图灵班) in Zhejiang University.",
     ],
